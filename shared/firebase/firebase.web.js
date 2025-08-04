16c169ce29b8fe61ed1ab2ab522ae8f70185b748
Soo Yeon Ahn <soo.yeon.ahn2002@gmail.com>
2025-08-03 19:42:09 -0700

    rm simulink

--- shared/firebase/firebase.web.js
+++ shared/firebase/firebase.web.js
@@ -1,6 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApps } from 'firebase/app';
+import {
+  getAuth,
+  initializeAuth,
+  indexedDBLocalPersistence
+} from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,7 +15,11 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
+initializeAuth(app, {
+  persistence: indexedDBLocalPersistence
+});
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
